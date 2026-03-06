17a98546ddeff0813f00ac4952f7ea062ec5fef8
Yulia <giuliabrtn07@gmail.com>
2026-03-07 02:52:41 +0300

    handle missing values in Sleep Disorder

--- lab1/preprocessing.py
+++ lab1/preprocessing.py
@@ -24,3 +24,16 @@ print(df.columns.tolist())
 missing = df.isnull().sum()
 print("\nПропущенные значения по столбцам:")
 print(missing[missing > 0])
+
+
+#4. Заполнить пропущенные значения в датасете модой/медианой/средним значением и показать, что они действительно были заполнены
+print("\nУникальные значения в Sleep Disorder до заполнения:")
+print(df['Sleep Disorder'].value_counts(dropna=False))
+
+mode_sleep = df['Sleep Disorder'].mode()[0]
+df['Sleep Disorder'] = df['Sleep Disorder'].fillna(mode_sleep)   # исправлено!
+
+print(f"\nСтолбец Sleep Disorder заполнен модой: '{mode_sleep}'")
+
+print("\nПропуски после заполнения:")
+print(df.isnull().sum())
